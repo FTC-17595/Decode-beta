65c4671c4a7bca952baf795dbe4246fb23ff6617
Anirudh Vikram <anivikr@gmail.com>
2025-10-16 16:20:16 -0700

    Revert "AprilTag"
    This reverts commit f54ef7f151b15d6d5c55d59a48048c305d1b9b45.

--- TeamCode/src/main/java/Intake.java
+++ TeamCode/src/main/java/Intake.java
@@ -5,39 +5,35 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class Intake {
+
     private final DcMotor intakeSpin;
     private final LinearOpMode linearOpMode;
 
     public Intake(LinearOpMode linearOpMode) {
-        this.linearOpMode = linearOpMode;
         this.intakeSpin = linearOpMode.hardwareMap.dcMotor.get("intakeSpin");
+        this.linearOpMode = linearOpMode;
+
     }
 
     public void configureMotorModes() {
         intakeSpin.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
         intakeSpin.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
-    public void intakeArtifactSlow(boolean intakeArtifactSlow) {
-        if (intakeArtifactSlow) {
-            intakeSpin.setPower(0.2);
-            linearOpMode.sleep(2500);
-        } else if (linearOpMode.isStopRequested()) {
-            intakeSpin.setPower(0);
-        }
-    }
-
-    public void intakeArtifact(boolean intakeArtifact) {
-        if (intakeArtifact) {
+    public void inttakeArtifact(boolean intakeArtifact) {
+        if (inttakeArtifact) {
             intakeSpin.setPower(0.8);
-            linearOpMode.sleep(2500);
-        } else if (linearOpMode.isStopRequested()) {
-            intakeSpin.setPower(0);
+            sleep(2500);
+        } else {
+             intakeSpin.setPower(0);
         }
     }
 
     public void displayTelemetry() {
         linearOpMode.telemetry.addData("Intake Motor Power", intakeSpin.getPower());
         linearOpMode.telemetry.update();
+
+    
     }
 }
