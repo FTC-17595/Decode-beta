f03a5789fa11e34dfa93973ea785dfa864f75d33
Anirudh Vikram <anivikr@gmail.com>
2025-11-01 13:30:12 -0700

    Push for team to get up to date with current Auto progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoConstants.java
@@ -1,12 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 public class AutoConstants {
+    private LinearOpMode linearOpMode;
+    GoBildaPinpointDriver odo;
     static final double DRIVE_SPEED = 0.5;
     static final double TURN_SPEED = 0.5;
     static final double STRAFE_SPEED = 0.5;
     static final double LAUNCH_EXIT_SPEED = 0.8;
-    static final double CLASSIFIER_X = 210 ;
-    static final double CLASSIFIER_Y = 160;
-    static final double AUTO_ARTIFACT_SHOOT_POWER = 0.8;
+    static final double CLASSIFIER_X = 1191.53394;
+    static final double CLASSIFIER_Y = -265.373;
+    static final double AUTO_ARTIFACT_SHOOT_POWER = 0.72;
     static final double AUTO_STRAFE_POWER = 0.5;
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeLM1Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeLM1Auto.java
@@ -61,21 +61,12 @@ public class DecodeLM1Auto extends LinearOpMode {
             ArtifactHandlingSystem artifactSystem = new ArtifactHandlingSystem(linearOpMode);
             artifactSystem.shootAutoArtifact();
 
-        driveToPos(40, 23);
-
-        while (counter < 2) {
-            frontLeftMotor.setPower(AutoConstants.DRIVE_SPEED);
-            backLeftMotor.setPower(AutoConstants.DRIVE_SPEED);
-            frontRightMotor.setPower(AutoConstants.DRIVE_SPEED);
-            backRightMotor.setPower(AutoConstants.DRIVE_SPEED);
-        }
-        frontLeftMotor.setPower(0);
-        backLeftMotor.setPower(0);
-        frontRightMotor.setPower(0);
-        backRightMotor.setPower(0);
 
 
 
+
+
+/*      TODO: ADD IN THE VALUES FOR  OBELISK
         if (PPG == true) {
             driveToPos(12,12);
             // the PPG line
@@ -88,7 +79,7 @@ public class DecodeLM1Auto extends LinearOpMode {
             driveToPos(12,12);
         }
 
-
+*/
     }
 
     // Drives the robot toward a given (X, Y) coordinate using odometry and IMU heading
